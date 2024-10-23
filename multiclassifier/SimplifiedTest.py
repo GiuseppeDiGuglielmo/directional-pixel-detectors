8fa26343ca07df15b94a6a1ffa4da9d9a7a1ee35
Anthony Badea <abadea@fasic-137645.fnal.gov>
2024-10-23 11:01:16 -0500

    simplify the code and evaluate on the test inputs

--- multiclassifier/SimplifiedTest.py
+++ multiclassifier/SimplifiedTest.py
@@ -41,40 +41,6 @@ def CreateModel(shape, nb_classes, first_dense):
     model = Model(inputs=x_in, outputs=x)
     return model
 
-# convert 8 number y-profile into 16 bits by quantizing each value
-def quantizeInputs(x):
-    
-    # define mapping for encoding into 2 bit inputs
-    # X_train: max= 25 , log2(max)= 5 , min= 0
-    # X_test: max= 21 , log2(max)= 5 , min= 0
-    max_value_X_train = 25 # np.max(X_train)
-    min_value_X_train = 0 # np.min(X_train)
-    max_value_X_test = 21 # np.max(X_test)
-    min_value_X_test = 0 # np.min(X_test)
-    log2_max_value_X_train = int(np.ceil(math.log2(np.abs(max_value_X_train))))
-    log2_max_value_X_test = int(np.ceil(math.log2(np.abs(max_value_X_test))))
-    print('X_train: max=', max_value_X_train, ', log2(max)=', log2_max_value_X_train, ', min=', min_value_X_train)
-    print('X_test: max=', max_value_X_test, ', log2(max)=', log2_max_value_X_test, ', min=', min_value_X_test)
-
-    # set threshold values
-    threshold1 = 10  
-    threshold2 = 20 
-
-    # update with logic for how to convert y-profile of 8 numbers into 16 numbers
-    x_quant = []
-    for arr in x:
-        x_quant.append([])
-        for i in arr:
-             bit1 = 1 if i > threshold1 else 0
-             bit2 = 1 if i > threshold2 else 0
-             x_quant[-1].append(bit1)
-             x_quant[-1].append(bit2)
-
-    # convert to np array
-    x_quant = np.array(x_quant)
-
-    return x_quant
-
 if __name__ == "__main__":
     
     # create model
@@ -86,24 +52,16 @@ if __name__ == "__main__":
 
     # load the model
     model_file = "/fasic_home/gdg/research/projects/CMS_PIX_28/directional-pixel-detectors/multiclassifier/models/ds8l0_padded_noscaling_keras_d58model.h5"
-    # co = {}
-    # utils._add_supported_quantized_objects(co)
-    model = tf.keras.models.load_model(model_file) #, custom_objects=co)
+    model = tf.keras.models.load_model(model_file)
     
-    # define some test input data
-    x_test = [
-        [0, 0, 5, 15, 15, 7, 0, 0], # row 7-0 of an example y-profile that is 8 numbers long because we have a 8x32
-    ]
-    y_test = [2]
-    
-    # convert to np and print shapes
-    x_test = np.array(x_test)
-    y_test = np.array(y_test)
-    
-    # quantize -> NEEDS TO BE UPDATED
-    x_test = quantizeInputs(x_test)
-    print(x_test)
-    print(y_test)
+    # load example inputs and outputs
+    x_test = pd.read_csv("/asic/projects/C/CMS_PIX_28/benjamin/verilog/workarea/cms28_smartpix_verification/PnR_cms28_smartpix_verification_D/tb/dnn/csv/l6/input_1.csv", header=None)
+    x_test = np.array(x_test.values.tolist())
+
+    y_test = pd.read_csv("/asic/projects/C/CMS_PIX_28/benjamin/verilog/workarea/cms28_smartpix_verification/PnR_cms28_smartpix_verification_D/tb/dnn/csv/l6/layer7_out.csv", header=None)
+    y_test = np.array(y_test.values.tolist()).flatten()
+
+    print(x_test.shape, y_test.shape)
 
     # get loss, accuracy
     loss, accuracy = model.evaluate(x_test, y_test)
