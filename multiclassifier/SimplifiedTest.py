4a5b226372d7300a074d46d8ecf034b4024a3217
Anthony Badea <abadea@fasic-137645.fnal.gov>
2024-12-06 17:11:41 -0600

    working to get chip inputs

--- multiclassifier/SimplifiedTest.py
+++ multiclassifier/SimplifiedTest.py
@@ -5,6 +5,9 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 # Standard library imports
 import math
 import h5py
+from fxpmath import Fxp
+import pandas as pd
+import csv
 
 # Third-party imports
 import matplotlib.pyplot as plt
@@ -59,6 +62,48 @@ def CreateQModel(shape, nb_classes):
     model = Model(inputs=x_in, outputs=x)
     return model
 
+# convert code from hsl4ml style output to chip style input
+def prepareWeights(path):
+    data_fxp = Fxp(None, signed=True, n_word=4, n_int=0)
+    data_fxp.rounding = 'around'
+    def to_fxp(val):
+        return data_fxp(val)
+
+    b5_data = pd.read_csv(os.path.join(path, 'b5.txt'), header=None)
+    w5_data = pd.read_csv(os.path.join(path, 'w5.txt'), header=None)
+    b2_data = pd.read_csv(os.path.join(path, 'b2.txt'), header=None)
+    w2_data = pd.read_csv(os.path.join(path, 'w2.txt'), header=None)
+    print(b5_data)
+    
+    b5_data_list = []
+    w5_data_list = []
+    b2_data_list = []
+    w2_data_list = []
+    
+    for i in range(2, -1, -1):
+        b5_data_list.append(to_fxp(b5_data.values[0][i]).bin())
+
+    for i in range(173, -1, -1):
+        w5_data_list.append(to_fxp(w5_data.values[0][i]).bin())
+
+    for i in range(57, -1, -1):
+        b2_data_list.append(to_fxp(b2_data.values[0][i]).bin())
+
+    for i in range(927, -1, -1):
+        w2_data_list.append(to_fxp(w2_data.values[0][i]).bin())
+
+    b5_bin_list = [int(bin_string) for data in b5_data_list for bin_string in data]
+    w5_bin_list = [int(bin_string) for data in w5_data_list for bin_string in data]
+    b2_bin_list = [int(bin_string) for data in b2_data_list for bin_string in data]
+    w2_bin_list = [int(bin_string) for data in w2_data_list for bin_string in data]
+    pixel_list = [0 for _ in range(512)]
+    b5_w5_b2_w2_pixel_list = b5_bin_list + w5_bin_list + b2_bin_list + w2_bin_list + pixel_list
+
+    csv_file = os.path.join(path, 'b5_w5_b2_w2_pixel_bin.csv')
+    with open(csv_file, mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(b5_w5_b2_w2_pixel_list)
+
 if __name__ == "__main__":
 
     # create model
@@ -99,6 +144,8 @@ if __name__ == "__main__":
         # Convert to an hls model
         hls_model = hls4ml.converters.convert_from_keras_model(model, hls_config=config, output_dir='test_prj')
         hls_model.write()
+        # prepare weights
+        prepareWeights("test_prj/firmware/weights/")
 
         
     # load example inputs and outputs
